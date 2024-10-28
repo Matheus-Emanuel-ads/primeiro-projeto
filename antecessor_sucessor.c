7522a089d7822d024f5d1796f1a4db61fdf25fc1
Matheus-Emanuel-UCB <matheusemanuel.corporativo@gmail.com>
2024-10-28 16:17:38 -0300

    Create antecessor_sucessor.c

---
+++ antecessor_sucessor.c
@@ -0,0 +1,18 @@
+// Antecessor e Sucessor
+#include<stdio.h>
+
+int main() {
+
+	int numero;
+	int nmenos;
+	int nmais;
+
+	printf("Digite um numero e receba seu antecedor e sucessor:");
+	scanf("%d", &numero);
+
+	nmenos = numero - 1;
+	nmais = numero + 1;
+
+	printf("Antecedor: %d \n", nmenos);
+	printf("Sucessor: %d \n", nmais);
+}
